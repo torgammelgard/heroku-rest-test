2eb6cfd07c54bb304ccc5e38bee210d29d95cf8a
Tor Gammelgård <tor.gammelgard@gmail.com>
2018-11-20 17:14:00 +0100

    Initial commit

---
+++ src/main/java/se/torgammelgard/demo/DemoApplication.java
@@ -0,0 +1,12 @@
+package se.torgammelgard.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
