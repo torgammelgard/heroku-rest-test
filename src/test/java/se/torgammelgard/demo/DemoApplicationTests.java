61a4eea25592f9abc113a95543fbf20fc13cbe01
Tor Gammelgård <tor.gammelgard@gmail.com>
2018-11-21 15:47:12 +0100

    Remove tests

--- src/test/java/se/torgammelgard/demo/DemoApplicationTests.java
+++ src/test/java/se/torgammelgard/demo/DemoApplicationTests.java
@@ -2,19 +2,8 @@ package se.torgammelgard.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.Assert;
-
-import javax.sql.DataSource;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
-import static org.springframework.util.Assert.notNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -24,27 +13,5 @@ public class DemoApplicationTests {
     public void contextLoads() {
 
     }
-
-    @Value("spring.datasource.url")
-    private String dbUrl;
-
-
-    @Autowired
-    private DataSource dataSource;
-
-    @Test
-    public void checkDbUrl() {
-        notNull(dataSource);
-        try (Connection connection = dataSource.getConnection()) {
-            Statement stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * from test");
-            while (rs.next()) {
-                System.out.println(rs.getString(0));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
 
 }
